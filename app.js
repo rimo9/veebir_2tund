39fb971227437ed604f381e906d91befd1ed340b
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2016-02-10 10:49:33 +0200

    added singleton pattern

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//a
+(function(){
+  "use strict";
+
+  var Moosipurk = function(){
+    // SINGLETON PATTERN (4 rida)
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
+    Moosipurk.instance = this; //this viitab moosipurgile
+
+    console.log('moosipurgi sees');
+  };
+
+  window.onload = function(){
+    var app = new Moosipurk();
+    var app2 = new Moosipurk();
+  };
+
+})();
